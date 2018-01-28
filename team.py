bc3e5e044d8b21f91568008f0f298ac00121a960
Kevin Ndung'u <kevgathuku@gmail.com>
2018-01-28 20:31:55 +0300

    Populate all team methods with correct response

--- team.py
+++ team.py
@@ -3,31 +3,40 @@ class Team(object):
     def __init__(self, client):
         self.client = client
 
-    def list_for_org(self, org_id):
-        # path = f"/orgs/{org_id}/teams"
-        response = self.client.orgs(org_id).teams.GET()
+    def list_for_org(self, org_username):
+        # GET /orgs/{org_username}/teams
+        response = self.client.orgs(org_username).teams.GET()
         return response.json()
 
     def list_for_project(self, project_id):
-        # /projects/{project_id}/teams
+        # GET /projects/{project_id}/teams
         response = self.client.projects(project_id).teams.GET()
         return response.json()
 
     def list_for_secret(self, secret_id):
-        # /secrets/{secret_id}/teams
+        # GET /secrets/{secret_id}/teams
         response = self.client.secrets(secret_id).teams.GET()
         return response.json()
 
-    def create_for_org(self, org_id):
-        pass
+    def create_for_org(self, org_id, params=None):
+        # POST /orgs/{org_username}/teams
+        # TODO: Pass params to POST request
+        response = self.client.orgs(org_id).POST()
+        return response.json()
 
     def get(self, team_id):
-        # /teams/{id}
+        # GET /teams/{id}
         response = self.client.teams(team_id).GET()
         return response.json()
 
-    def update(self, team_id):
-        pass
+    def update(self, team_id, params=None):
+        # PATCH /teams/{id}
+        # TODO: Pass params to PATCH
+        response = self.client.teams(team_id).PATCH(params)
+        return response.json()
 
     def delete(self, team_id):
-        pass
+        # DELETE /teams/{id}
+        # TODO: Figure out correct response
+        response = self.client.teams(team_id).DELETE()
+        return response.json()
